51a94b57cdbd1f0e1ad02608f120000477cdf1fc
Grigoriy <foxtail25@gmail.com>
2023-10-24 11:15:42 +0300

    1111

--- src/components/ObjAddToArr.js
+++ src/components/ObjAddToArr.js
@@ -1,7 +1,44 @@
 // Реактивное добавление в массив объектов в React
 
+import { nanoid } from "nanoid";
+import { useState } from "react";
+
+const initNotes = [
+    {
+        id: 'GYi9G_uC4gBF1e2SixDvu',
+        prop1: 'value11',
+        prop2: 'value12',
+        prop3: 'value13',
+    },
+    {
+        id: 'IWSpfBPSV3SXgRF87uO74',
+        prop1: 'value21',
+        prop2: 'value22',
+        prop3: 'value23',
+    },
+    {
+        id: 'JAmjRlfQT8rLTm5tG2m1L',
+        prop1: 'value31',
+        prop2: 'value32',
+        prop3: 'value33',
+    },
+];
+
+
 export default function ObjAddToArr() {
-    return (
-        <p>bum</p>
+    
+    const [notes, setNotes] = useState(initNotes)
+    
+    let result = notes.map(el => <li key={el.id}>
+        <span>{el.prop1} </span>
+        <span>{el.prop2} </span>
+        <span>{el.prop3} </span>
+    </li>)
+
+return (
+    <ul>
+            {result}
+            {nanoid(21)}
+        </ul>
     )
 }
