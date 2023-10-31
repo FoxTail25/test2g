1a15fa9e002d624bd14301d329a96f9b9e67e3f6
Grigoriy <foxtail25@gmail.com>
2023-10-31 09:38:39 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
 import { UsEffect } from "./components/hoocks/usEffect/usEffect";
 // import { StyleComp } from "./components/styled/styledComp/styleComp";
-=======
-
-import { Task2 } from "./components/styled/styledcomp/Task2";
-// import { Task1 } from "./components/styled/styledcomp/task1";
-
-
->>>>>>> 646550708d393ebea9f28135676200b74b4712fb
 
 
 export default function App() {
 
-<<<<<<< HEAD
 
 	return (
 		<UsEffect/>
-=======
-	return (
-		<Task2/>
->>>>>>> 646550708d393ebea9f28135676200b74b4712fb
 	);
 }
